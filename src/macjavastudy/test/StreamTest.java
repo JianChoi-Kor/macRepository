4bc89f1e39a68a8a44cb38e092a98944bdee0ca6
JianChoi-Kor <wldks79@gmail.com>
2020-12-17 02:29:38 +0900

    20201217

---
+++ src/macjavastudy/test/StreamTest.java
@@ -0,0 +1,12 @@
+package macjavastudy.test;
+
+import java.util.ArrayList;
+
+public class StreamTest {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> list = new ArrayList<>();
+	}
+
+}
