90426c64da7149969406bb841c568e3aaea97632
JianChoi-Kor <wldks79@gmail.com>
2020-12-17 23:26:47 +0900

    20201217

--- src/macjavastudy/test/StreamTest.java
+++ src/macjavastudy/test/StreamTest.java
@@ -1,12 +1,23 @@
 package macjavastudy.test;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StreamTest {
 
 	public static void main(String[] args) {
+		String[] strArray = {"A", "B", "C", "D"};
+		Stream<String> strStream = Arrays.stream(strArray);
+		strStream.forEach(a -> System.out.println("strArray : "+a));
 		
-		ArrayList<Integer> list = new ArrayList<>();
+		System.out.println();
+		
+		int[] intArray = {4, 3, 2, 1};
+		IntStream intStream = Arrays.stream(intArray);
+		intStream.forEach(n -> System.out.println("intArray : "+n));
 	}
-
 }
+
+
+
