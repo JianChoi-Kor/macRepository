5d104a8d11b728b1c6d18aad4cb2309621f48216
JianChoi-Kor <wldks79@gmail.com>
2020-12-27 23:45:07 +0900

    20201227

---
+++ src/macjavastudy/servlet/Nana.java
@@ -0,0 +1,15 @@
+package macjavastudy.servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class Nana extends HttpServlet
+{
+	public void service(HttpServletRequest request
+							, HttpServletResponse response)
+							throws IOException, ServletException
+							{
+		System.out.println("hello Selvelt");
+							}
+}
