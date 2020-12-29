bbde68f3b5056f2f101be432ee92db9899f78f90
JianChoi-Kor <wldks79@gmail.com>
2020-12-29 14:06:57 +0900

    20201229

--- JSP/src/com/newlecture/web/Nana.java
+++ JSP/src/com/newlecture/web/Nana.java
@@ -3,17 +3,33 @@ package com.newlecture.web;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+@WebServlet("/hi")
 public class Nana extends HttpServlet {
 	
 	@Override
 	public void service(HttpServletRequest req
 						, HttpServletResponse resp) throws IOException {
 		
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html; charset=UTF-8");
+		
 		PrintWriter out = resp.getWriter();
-		out.println("helloooooo");
+		
+		String cnt_ = req.getParameter("cnt");
+
+		
+		int cnt = 100;
+		if(cnt_ != null && !cnt_.equals(""))
+			cnt = Integer.parseInt(cnt_);
+		
+		for(int i=0; i<cnt; i++) {
+			out.println((i+1)+": 안뇽 Servlet!!<br/>");
+		}
 	}
 }
