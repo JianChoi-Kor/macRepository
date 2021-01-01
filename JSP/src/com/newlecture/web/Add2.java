c3d71b0859e4bd01aadfb2d6484dcf2cdaa454cc
JianChoi-Kor <wldks79@gmail.com>
2021-01-02 02:34:43 +0900

    20210101

---
+++ JSP/src/com/newlecture/web/Add2.java
@@ -0,0 +1,36 @@
+package com.newlecture.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/add2")
+public class Add2 extends HttpServlet {
+	
+	@Override
+	public void service(HttpServletRequest req
+						, HttpServletResponse resp) throws IOException {
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html; charset=UTF-8");
+		
+		PrintWriter out = resp.getWriter();
+		
+		String[] num_ = req.getParameterValues("num");
+		
+		int result = 0;
+		
+		for(int i=0; i<num_.length; i++) {
+			int num = Integer.parseInt(num_[i]);
+			result += num;
+		}
+		
+		out.printf("result : %d\n", result);
+		
+	}
+}
+
+
