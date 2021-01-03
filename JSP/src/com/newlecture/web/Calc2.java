daf38c8e62a0fb2974b3df05a845d553f913e951
JianChoi-Kor <wldks79@gmail.com>
2021-01-04 01:10:33 +0900

    20210104

--- JSP/src/com/newlecture/web/Calc2.java
+++ JSP/src/com/newlecture/web/Calc2.java
@@ -3,10 +3,12 @@ package com.newlecture.web;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletContext;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/calc2")
 public class Calc2 extends HttpServlet {
@@ -14,29 +16,47 @@ public class Calc2 extends HttpServlet {
 	@Override
 	public void service(HttpServletRequest req
 						, HttpServletResponse resp) throws IOException {
+		
+		//ServletContext application = req.getServletContext();
+		HttpSession session = req.getSession();
+		
 		resp.setCharacterEncoding("UTF-8");
 		resp.setContentType("text/html; charset=UTF-8");
 		
 		PrintWriter out = resp.getWriter();
 		
-		String x_ = req.getParameter("x");
-		String y_ = req.getParameter("y");
+		String v_ = req.getParameter("v");
 		String op = req.getParameter("operator");
 		
-		int x = 0;
-		int y = 0;
 		
-		if(!x_.equals("")) x = Integer.parseInt(x_);
-		if(!y_.equals("")) y = Integer.parseInt(y_);
+		int v = 0;
+		if(!v_.equals("")) v = Integer.parseInt(v_);
 		
-		int result = 0;
 		
-		if(op.equals("덧셈")) 
-		result = x+y;
-		else
-		result = x-y;
+		if(op.equals("=")) {
+			
+			//int x = (Integer)application.getAttribute("value");
+			int x = (Integer)session.getAttribute("value");
+			int y = v;
+			//String operator = (String)application.getAttribute("op");
+			String operator = (String)session.getAttribute("op");
+			
+			int result = 0;
+			
+			if(operator.equals("+")) 
+				result = x+y;
+			else 
+				result = x-y;
+			
+			out.printf("result : %d \n", result);
+			
+		} else {
+			//application.setAttribute("value", v);
+			//application.setAttribute("op", op);
+			session.setAttribute("value", v);
+			session.setAttribute("op", op);
+		}
 		
-		out.printf("result : %d \n", result);
 	}
 		
 }
